57e3fd7d155a92b2bca50d3aa3554559a2090057
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-02-06 00:19:03 +0530

    merge sorted arrays

--- questions/question2.cpp
+++ questions/question2.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 using namespace std;
 
+void mergeArray(int *arr1, int n1, int *arr2, int n2, int *arr3, int n3){
+    int i = 0;
+    int j = 0;
+    int k = 0; 
+    while(i < n1 and j < n2){
+        if(arr1[i] < arr2[j]){
+            arr3[k++] = arr1[i++];
+        }else{
+            arr3[k++] = arr2[j++];
+        }
+    }
+
+    while(i < n1){
+        arr3[k++] = arr1[i++];
+    }
+
+    while(j < n2){
+        arr3[k++] = arr2[j++];
+    }
+
+}
+
 int main(){
     // Given two sorted arrays, Write a program to merge them in a sorted manner
     int num1[] = {5, 8, 10, 11, 12};
@@ -12,6 +34,7 @@ int main(){
     int n3 = n1+n2;
     int num3[n3];
 
+    mergeArray(num1, n1, num2, n2, num3, n3);
     for(int i = 0; i < n3; i++){
         cout<<num3[i]<<" ";
     }
